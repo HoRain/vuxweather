587253818e63789e038c59ec7a8d32dfae078f4a
HorainChung <horainchung@outlook.com>
2017-03-22 11:23:31 +0000

    init

---
+++ vuxweather/webserver.js
@@ -0,0 +1,7 @@
+// JavaScript File
+const express = require('express');
+const app = express();
+app.use(express.static('dist'));
+const server = app.listen(8080, function () {
+    console.log("Web server is running, port is 8080")
+})
